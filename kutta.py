be83eb90e80ceb5299b3ea2e17cc8742f304fdae
David <david.louapre@gmail.com>
2019-08-04 07:27:19 +0200

    Before producing

--- kutta.py
+++ kutta.py
@@ -12,17 +12,19 @@ import matplotlib.animation as animation
 plt.rcParams['animation.ffmpeg_path'] = r'/Volumes/Data/Youtube/[ffmpeg]/ffmpeg'
 
 # for complex number operations
-from cmath import exp, pi, log, sqrt, phase
+from cmath import exp, pi, log, sqrt
+
+
 
 U0 = 1              # Velocity
 a = 1               # Radius
 b = 0.05            # Joukowski shift
-c = 0.9           #        Joukowski deformation
+c = 0.9             # Joukowski deformation
 alpha = 15/180*pi   # Angle of the flow
 
 gamma = -4*pi*U0*(a+b)*np.sin(alpha)   # Circulation to get Kutta condition
 theta = alpha                          # Global rotation so that the flow is horizontal
-z0 = -b
+z0 = -b                                # Shift of circle 
 
 
 def circlePot(z):
@@ -37,7 +39,6 @@ def rotTheta(z):
 
 def invTheta(z):
     return exp(1j*theta)*z
-
 
 def Jouk(z):
     return z + c**2/z
@@ -59,7 +60,7 @@ invConfMap = invJouk
 
 # Range in X and Y    
 W = 6
-Y, X = np.mgrid[-W:W:100j, -W:W:100j]   #300
+Y, X = np.mgrid[-W:W:300j, -W:W:300j]
 Z = X + 1j * Y
 
 
@@ -92,11 +93,11 @@ P = - (U**2 + V**2) + U0**2
 ########
 
 # Plot
-fig = plt.figure(figsize=(16,16),dpi=50)  #200
+fig = plt.figure(figsize=(16,16),dpi=200)
 ax = fig.add_subplot(111)
 
 # Seeds for the stream lines
-K = 20   #80
+K = 60
 seeds = np.array([[-0.99*W]*K, np.linspace(-0.99*W,0.99*W,K)])
 
 # Optionnaly color 
@@ -106,7 +107,7 @@ normalizer = matplotlib.colors.Normalize(0,2)
 # col = "black"
 # Density high enough to make sure all lines are ploted
 ax.streamplot(X, Y, U, V, start_points = seeds.T, integration_direction = 'forward', 
-              density=10, linewidth=2, color = col, norm=normalizer,cmap="jet", arrowstyle="-") # "seismic"
+              density=10, linewidth=2, color = col, norm=normalizer,cmap="jet", arrowstyle="-")
 #ax.streamplot(X, Y, U, V, start_points = seeds.T, integration_direction = 'forward', 
 #              density=10, linewidth=4, color="black", arrowstyle="-") # "seismic"
 
@@ -115,7 +116,7 @@ ax.set_xlim(-W,W)
 ax.set_ylim(-W,W)
 
 # Add a mask (at higher resolution for the shape of the wing)
-Yr, Xr = np.mgrid[-W:W:400j, -W:W:400j]   #2000
+Yr, Xr = np.mgrid[-W:W:3000j, -W:W:3000j]
 Zr = Xr + 1j*Yr
 mask = np.zeros(Zr.shape,dtype=bool)
 level = np.zeros(Zr.shape)
@@ -129,7 +130,7 @@ mask = mask[:,::-1]
 level = level[:,::-1]
 
 #ax.imshow(level, extent=(-W, W, -W, W), alpha=1, cmap='jet', clim = (0,4), aspect='auto') 
-ax.imshow(P[::-1,:], extent=(-W, W, -W, W), alpha=0.6, cmap='bwr', clim = (-2,2), aspect='auto')
+#ax.imshow(P[::-1,:], extent=(-W, W, -W, W), alpha=0.6, cmap='bwr', clim = (-2,2), aspect='auto')
 immask = np.zeros((mask.shape[0], mask.shape[1],4),dtype=float)
 
 for i in range(immask.shape[0]):
@@ -152,35 +153,35 @@ ax.imshow(immask, extent=(-W, W, -W, W), aspect='auto')
 #ax.plot(xs,ys,'--')
         
 ax.set_aspect(1)
+plt.savefig("test.png")
 
-OUTPUT = True
-name = "test"
-NFRAMES = 300
-FPS = 30
-STEP = 50
-DT = 0.001
+#OUTPUT = True
+#name = "test"
+#NFRAMES = 300
+#FPS = 30
+#STEP = 50
+#DT = 0.001
+#
+#particles = [np.r_[seeds[0,k],seeds[1,k]] for k in range(K)]
+#
+#trajectories = [ax.plot(x[0],x[1],'or')[0] for x in particles]
+#
+#def update(i):
+#    print("Frame "+str(i))
+#    for k in range(len(particles)):
+#        for s in range(STEP):
+#            zpart = particles[k][0] + 1j * particles[k][1]
+#            zvel = complexVelocity(zpart)
+#            particles[k][0] += zvel.real * DT
+#            particles[k][1] += - zvel.imag * DT
+#        trajectories[k].set_data(particles[k][0],particles[k][1])
+#
+#ani = animation.FuncAnimation(fig, update, frames = NFRAMES)
+#
+#if OUTPUT:    
+#    writer = animation.FFMpegWriter(fps=FPS, bitrate = None)
+#    ani.save(name+".mp4", writer = writer, savefig_kwargs={'facecolor':'black'})
+#else:
+#    plt.show() 
 
-particles = [np.r_[seeds[0,k],seeds[1,k]] for k in range(K)]
 
-trajectories = [ax.plot(x[0],x[1],'or')[0] for x in particles]
-
-def update(i):
-    print("Frame "+str(i))
-    for k in range(len(particles)):
-        for s in range(STEP):
-            zpart = particles[k][0] + 1j * particles[k][1]
-            zvel = complexVelocity(zpart)
-            particles[k][0] += zvel.real * DT
-            particles[k][1] += - zvel.imag * DT
-        trajectories[k].set_data(particles[k][0],particles[k][1])
-
-ani = animation.FuncAnimation(fig, update, frames = NFRAMES)
-
-if OUTPUT:    
-    writer = animation.FFMpegWriter(fps=FPS, bitrate = None)
-    ani.save(name+".mp4", writer = writer, savefig_kwargs={'facecolor':'black'})
-else:
-    plt.show() 
-
-
-#plt.savefig("test.png")
